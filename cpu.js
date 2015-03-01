b28e928f8494d1666a8024410885591c6bb3f18f
asteriskman7 <asteriskman@gmail.com>
2015-02-28 21:45:19 -0500

    start basic cpu object

--- cpu.js
+++ cpu.js
@@ -1,2 +1,17 @@
-var cpu = {
+
+function Cpu() {
+  this.pc = 0;
+  this.sp = 0;
+  //R0-R7
+  this.regs = [0,0,0,0,0,0,0,0];
+  this.ram = [0,1,2,3,4,5,6,7];
+}
+
+Cpu.prototype.tick = function() {
+  var pcRam = this.ram[this.pc];
+
+  this.regs[0] = pcRam;
+
+  this.pc = (this.pc + 1) & 0xFFFF;
+
 };
