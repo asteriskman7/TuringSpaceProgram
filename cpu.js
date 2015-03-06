b0e64c55aea220c3d0544cbfb7ebd74cc0b9567d
asteriskman7 <asteriskman@gmail.com>
2015-03-06 12:47:42 -0500

    implement load/store opcodes and accompaning tests

--- cpu.js
+++ cpu.js
@@ -1,23 +1,122 @@
+'use strict';
 
 function Cpu() {
-  //R0-R7, PC, SP, AF, IM, SEG, JD, ?, ?, ?, ZERO
+  //random seed used in this.rnd16bit();
+  this.seed = 1;
+
+
+  //R0-R7, PC, SP, AF, IM, SEG, JD, ?, ZERO
   this.regs = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-  this.ram = [0,1,0,2,0,3,0,4,0,5,0,6];
+  this.regMap = {
+    R0: 0, 
+    R1: 1,
+    R2: 2,
+    R3: 3,
+    R4: 4,
+    R5: 5,
+    R6: 6,
+    R7: 7,
+    PC: 8,
+    SP: 9,
+    AF: 10,
+    IM: 11,
+    SEG: 12,
+    JD: 13,
+    ZERO: 15
+  };
+
+  this.reset();
 
   //overwrite this function for custom handling of cpu errors
-  this.runtimeError = function(msg) {};
+  this.runtimeError = function(msg) {console.log(msg);};
+
 }
+
+Cpu.prototype.reset = function() {
+  this.ram = [];
+  this.pci = this.regMap.PC;
+
+  var i;
+
+  for (i = 0; i <= 7; i++) {
+    this.regs[i] = this.rnd16bit();
+  }
+
+  this.regs[this.regMap.PC] = 0;
+  this.regs[this.regMap.SP] = 0;
+  this.regs[this.regMap.AF] = this.rnd16bit();
+  this.regs[this.regMap.IM] = this.rnd16bit();
+  this.regs[this.regMap.SEG] = this.rnd16bit();
+  this.regs[this.regMap.JD] = this.rnd16bit();
+  this.regs[this.regMap.ZERO] = 0;
+  
+};
 
 //cause the cpu to execute a single instruction
 Cpu.prototype.tick = function() {
-  var pcRam = this.readRam(this.pc);
+  var opcode = this.readRam(this.regs[this.pci]);
 
   //decode opcode in pcRam
-  //for now, all opcodes mean load next word into R0
-  var constVal = this.readRam(this.pc + 1);  
-  this.regs[0] = constVal;
-  this.pc = (this.pc + 1) & 0xFFFF;
-  this.pc = (this.pc + 1) & 0xFFFF;
+  var opClass = (opcode & 0xF000);
+  var opCmd = (opcode & 0x0F00);
+  var opArgA = (opcode & 0x00F0) >> 4;
+  var opArgB = (opcode & 0x000F);
+  var const8 = (opArgA << 4) | opArgB;
+
+  var pcJump = false;
+  var ramAddr;
+
+  switch (opClass) {
+    case 0x0000: //load/store
+      switch (opCmd) {
+        case 0x0000: //ld
+          this.regs[opArgA] = this.regs[opArgB];
+          break;
+        case 0x0200: //ldl
+          this.regs[0] = (this.regs[0] & 0xFF00) | const8;
+          break;
+        case 0x0300: //ldh
+          this.regs[0] = (this.regs[0] & 0x00FF) | (const8 << 8);
+          break;
+        case 0x0400: //ldr
+          this.regs[opArgA] = this.readRam(this.regs[opArgB]);
+          break;
+        case 0x0500: //str
+          this.ram[this.regs[opArgA]] = this.regs[opArgB];
+          break;
+        case 0x0600: //ldrc
+          ramAddr = (this.regs[this.regMap.SEG] + const8) & 0xFFFF;
+          this.regs[0] = this.readRam(ramAddr);
+          break;
+        case 0x0700: //strc
+          ramAddr = (this.regs[this.regMap.SEG] + const8) & 0xFFFF;
+          this.ram[ramAddr] = this.regs[0];
+          break;
+        case 0x0C00: //pop
+          this.regs[opArgA] = this.readRam(this.regs[this.regMap.SP]);
+          this.regs[this.regMap.SP] = (this.regs[this.regMap.SP] + 1) & 0xFFFF;
+          break;
+        case 0x0D00: //push
+          this.regs[this.regMap.SP] = (this.regs[this.regMap.SP] - 1) & 0xFFFF;
+          this.ram[this.regs[this.regMap.SP]] = this.regs[opArgA];
+          break;
+        default:
+          this.opcodeError(opcode);
+      }
+      break;
+    case 0x1000: //IO
+      break;
+    case 0x2000: //ALU
+      break;
+    case 0x3000: //JUMP
+      break;
+    default:
+      this.opcodeError(opcode);
+  }
+
+  if (!pcJump) {
+    this.regs[this.pci] += 1;
+  }
 
 };
 
@@ -26,5 +125,19 @@ Cpu.prototype.readRam = function(addr) {
   return this.ram[addr] || 0;
 };
 
-module.exports = Cpu;
+Cpu.prototype.rnd16bit = function() {
+  var x = Math.sin(this.seed++) * 10000;
+  var rnd = x - Math.floor(x);
+  return Math.floor(rnd * 0x10000);
+};
+
+Cpu.prototype.opcodeError = function(opcode) {
+  this.runtimeError('Illegal opcode ' + opcode.toString(16) + ' at ' + this.regs[this.pci].toString(16));
+};
+
+
+//only export if we're in the non-browser node environment
+if (typeof window === 'undefined') {
+  module.exports = Cpu;
+} 
 
--- mocha/cpu.js
+++ mocha/cpu.js
@@ -3,6 +3,24 @@ var Cpu = require('../cpu.js');
 
 var dut = new Cpu();
 
+var seed = Math.floor(Math.random() * 10000);
+
+function sinrnd() {
+  var x = Math.sin(seed++) * 10000;
+  return x - Math.floor(x);
+}
+
+
+function rnd16bit () {
+  return Math.floor(sinrnd() * 0x10000);
+}
+
+var iterations = 1;
+var i;
+
+
+console.log('Seed=' + seed);
+
 describe('Cpu', function() {
   describe('class', function() {
     it('should be a function', function() {
@@ -20,6 +38,259 @@ describe('Cpu', function() {
     it('should have 16 regs', function() {
       assert.equal(dut.regs.length, 16);
     });
+    it('should start with pc=0', function() {
+      assert.equal(dut.regs[8], 0);
+    });
+    it('should have an empty ram array', function() {
+      assert.equal(toString.call(dut.ram), '[object Array]');
+      assert.equal(dut.ram.length, 0);
+    });
+    it('should have a runtimeError callback', function() {
+      assert.equal(typeof dut.runtimeError, 'function');
+    });
   });
+
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('ld ('+i+')', function() {
+      var expectedVal = rnd16bit();
+      var srcReg = rnd16bit() & 0x7;
+      var dstReg = rnd16bit() & 0x7;
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0000 | srcReg | (dstReg << 4);
+        dut.regs[srcReg] = expectedVal;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + dstReg, function() {
+        assert.equal(dut.regs[dstReg], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Reg' + srcReg, function() {
+        assert.equal(dut.regs[srcReg], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('ldl ('+i+')', function() {
+      var origVal = rnd16bit();
+      var constVal = rnd16bit() & 0xFF;
+      var expectedVal = (origVal & 0xFF00) | constVal;
+      var dstReg = 0;
+      //console.log('Reg ' + dstReg + ' start ' + origVal + ' const ' + constVal + ' expectedVal ' + expectedVal);
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0200 | constVal;
+        dut.regs[dstReg] = origVal;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + dstReg, function() {
+        assert.equal(dut.regs[dstReg], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('ldh ('+i+')', function() {
+      var origVal = rnd16bit();
+      var constVal = rnd16bit() & 0xFF00;
+      var expectedVal = (origVal & 0x00FF) | constVal;
+      var dstReg = 0;
+      //console.log('Reg ' + dstReg + ' start ' + origVal + ' const ' + constVal + ' expectedVal ' + expectedVal);
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0300 | (constVal >> 8);
+        dut.regs[dstReg] = origVal;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + dstReg, function() {
+        assert.equal(dut.regs[dstReg], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('ldr ('+i+')', function() {
+      var origVal = rnd16bit();
+      var ramAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write addr 0 where this opcode will be
+      var expectedVal = rnd16bit();
+      var dstReg = rnd16bit() & 0x0007;
+      var addrReg; 
+      do {
+        addrReg = rnd16bit() & 0x0007;
+      } while (addrReg == dstReg);
+
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0400 | (dstReg << 4) | (addrReg);
+        dut.ram[ramAddr] = expectedVal;
+        dut.regs[dstReg] = origVal;
+        dut.regs[addrReg] = ramAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + dstReg, function() {
+        assert.equal(dut.regs[dstReg], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + ramAddr + ']', function() {
+        assert.equal(dut.ram[ramAddr], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('str ('+i+')', function() {
+      var origVal = rnd16bit();
+      var ramAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write addr 0 where this opcode will be
+      var expectedVal = rnd16bit();
+      var srcReg = rnd16bit() & 0x0007;
+      var addrReg; 
+      do {
+        addrReg = rnd16bit() & 0x0007;
+      } while (addrReg == srcReg);
+
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0500 | (addrReg << 4) | (srcReg);
+        dut.ram[ramAddr] = origVal;
+        dut.regs[srcReg] = expectedVal;
+        dut.regs[addrReg] = ramAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + srcReg, function() {
+        assert.equal(dut.regs[srcReg], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + ramAddr + ']', function() {
+        assert.equal(dut.ram[ramAddr], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('ldrc ('+i+')', function() {
+      var origVal = rnd16bit();
+      var expectedVal = rnd16bit();
+      var segAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write addr 0 where this opcode will be
+      var constAddr = rnd16bit() & 0x00FF;
+      var ramAddr = (segAddr + constAddr) & 0xFFFF;
+
+
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0600 | constAddr;
+        dut.ram[ramAddr] = expectedVal;
+        dut.regs[0] = origVal;
+        dut.regs[dut.regMap.SEG] = segAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + 0, function() {
+        assert.equal(dut.regs[0], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + ramAddr + ']', function() {
+        assert.equal(dut.ram[ramAddr], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('strc ('+i+')', function() {
+      var origVal = rnd16bit();
+      var expectedVal = rnd16bit();
+      var segAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write addr 0 where this opcode will be
+      var constAddr = rnd16bit() & 0x00FF;
+      var ramAddr = (segAddr + constAddr) & 0xFFFF;
+
+
+      before(function() {
+        dut = new Cpu();
+        dut.ram[0] = 0x0700 | constAddr;
+        dut.ram[ramAddr] = origVal;
+        dut.regs[0] = expectedVal;
+        dut.regs[dut.regMap.SEG] = segAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + 0, function() {
+        assert.equal(dut.regs[0], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + ramAddr + ']', function() {
+        assert.equal(dut.ram[ramAddr], expectedVal);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('pop ('+i+')', function() {
+      var origVal = rnd16bit();
+      var expectedVal = rnd16bit();
+      var spStartAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write data where this opcode will be
+      var spEndAddr = (spStartAddr + 1) & 0xFFFF;
+      var dstReg = rnd16bit() & 0x0007;
+
+      before(function() {
+        dut = new Cpu();
+        dut.regs[dstReg] = origVal;
+        dut.ram[0] = 0x0C00 | (dstReg << 4);
+        dut.ram[spStartAddr] = expectedVal;
+        dut.regs[dut.regMap.SP] = spStartAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + dstReg, function() {
+        assert.equal(dut.regs[dstReg], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + spStartAddr + ']', function() {
+        assert.equal(dut.ram[spStartAddr], expectedVal);
+      });
+      it('should contain ' + spEndAddr + ' in SP', function() {
+        assert.equal(dut.regs[dut.regMap.SP], spEndAddr);
+      });
+    });
+
+  }
+
+  for (i = 0; i < iterations; i++) {
+
+    describe('push ('+i+')', function() {
+      var origVal = rnd16bit();
+      var expectedVal = rnd16bit();
+      var spStartAddr = (rnd16bit() + 1) & 0xFFFF; //make sure to not write data where this opcode will be
+      if (spStartAddr === 1) {spStartAddr = 2;}
+      var spEndAddr = (spStartAddr - 1 ) & 0xFFFF;
+      var srcReg = rnd16bit() & 0x0007;
+
+      before(function() {
+        dut = new Cpu();
+        dut.regs[srcReg] = expectedVal;
+        dut.ram[0] = 0x0D00 | (srcReg << 4);
+        dut.ram[spEndAddr] = origVal;
+        dut.regs[dut.regMap.SP] = spStartAddr;
+        dut.tick();
+      });
+      it('should contain ' + expectedVal + ' in Reg' + srcReg, function() {
+        assert.equal(dut.regs[srcReg], expectedVal);
+      });
+      it('should contain ' + expectedVal + ' in Ram[' + spEndAddr + ']', function() {
+        assert.equal(dut.ram[spEndAddr], expectedVal);
+      });
+      it('should contain ' + spEndAddr + ' in SP', function() {
+        assert.equal(dut.regs[dut.regMap.SP], spEndAddr);
+      });
+    });
+
+  }
+
 });
 
