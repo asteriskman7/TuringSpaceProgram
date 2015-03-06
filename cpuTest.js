1c1ade1b145e88f14832996a6739fe0f2091eaa7
asteriskman7 <asteriskman@gmail.com>
2015-03-05 22:42:14 -0500

    adding more detail to cpu testing page

--- cpuTest.js
+++ cpuTest.js
@@ -1,6 +1,33 @@
 'use strict';
 
 var cputest = {
+  cpu: undefined,
+  div_cpu_info: undefined,
+
+  init: function() {
+    cputest.cpu = new Cpu();
+    cputest.div_cpu_info = document.getElementById('div_cpu_info');
+    
+    document.getElementById('button_step').onclick = cputest.step;
+    cputest.updateDisplay();
+
+  },
+
+  updateDisplay: function() {
+    var HTML = '<pre>\n';
+
+    HTML += 'PC: ' + cputest.cpu.pc + '\n';
+    HTML += 'REGS: ' + cputest.cpu.regs.toString() + '\n';
+    HTML += 'RAM: ' + cputest.cpu.ram.toString() + '\n';
+
+    HTML += '</pre>';
+    cputest.div_cpu_info.innerHTML = HTML;
+  },
+
+  step: function() {
+    cputest.cpu.tick();
+    cputest.updateDisplay();
+  },
 };
 
 window.onload = cputest.init;
