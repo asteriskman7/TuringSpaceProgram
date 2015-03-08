548c5e3731846767757d2c879922e6b589f3daf5
asteriskman7 <asteriskman@gmail.com>
2015-03-07 22:59:09 -0500

    create assembler.js and add to buildFileList

---
+++ assembler.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function Assembler() {
+  this.ram = [];
+  this.hex = '';
+}
+
+//take a string as input, assemble and enter the result in this.ram and this.hex
+Assembler.prototype.assemble = function(code) {
+
+}
+
